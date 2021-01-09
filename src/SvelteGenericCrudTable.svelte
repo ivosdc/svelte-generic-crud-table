dd87bee6bb4d8e59dffa06e82d4c708f0cc46d2a
ivosdc <ibozic@mail.de>
2021-01-09 20:39:26 +0100

    alternate line background

--- src/SvelteGenericCrudTable.svelte
+++ src/SvelteGenericCrudTable.svelte
@@ -201,7 +201,7 @@
                 <!-- /* istanbul ignore next line */ -->
                 {#each table_data as tableRow, i (tableRow)}
                     <div class="row"
-                         style="background-color:{i % 2 === 0 ? '#fefefe' : 'none'};min-height:{(table_config.row_settings !== undefined) && (table_config.row_settings.height !== undefined) ? table_config.row_settings.height : table_config_default.row_settings.height};">
+                         style="background-color:{i % 2 === 0 ? '#efefef' : '#ffffff'};min-height:{(table_config.row_settings !== undefined) && (table_config.row_settings.height !== undefined) ? table_config.row_settings.height : table_config_default.row_settings.height};">
                         {#each table_config.columns_setting as column_order, j}
                             {#each Object.entries(tableRow) as elem, k}
                                 <!-- /* istanbul ignore next */ -->
